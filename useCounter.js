3ed2e312bfe3dc0eea1ffcbd968c00c8b91cad02
Luis Martínez <luis.martinez@deus.ai>
2022-08-09 09:52:09 +0200

    initial commit

---
+++ useCounter.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export const useCounter = (initialValue = 1) => {
+  const [counter, setCounter] = useState(initialValue);
+
+  const increment = (value = 1) => {
+    setCounter((current) => current + value);
+  };
+
+  const decrement = (value = 1) => {
+    if (counter === 0) return;
+    setCounter((current) => current - value);
+  };
+
+  const reset = () => {
+    setCounter(initialValue);
+  };
+
+  return {
+    counter,
+    increment,
+    decrement,
+    reset,
+  };
+};
---
+++ useTodos.js
@@ -0,0 +1,46 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../../08-useReducer/todoReducer";
+
+const init = () => {
+  return JSON.parse(localStorage.getItem("todos")) || [];
+};
+
+export const useTodos = () => {
+  const [todos, dispatch] = useReducer(todoReducer, [], init);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const handleTodo = (todo) => {
+    const action = {
+      type: "Add Todo",
+      payload: todo,
+    };
+
+    dispatch(action);
+  };
+
+  const handleDeleteTodo = (id) => {
+    dispatch({
+      type: "Remove Todo",
+      payload: id,
+    });
+  };
+
+  const handleToggleTodo = (id) => {
+    dispatch({
+      type: "Toggle Todo",
+      payload: id,
+    });
+  };
+
+  return {
+    todos,
+    handleDeleteTodo,
+    handleToggleTodo,
+    handleTodo,
+    todosCount: todos.length,
+    pendingTodosCount: todos.filter((todo) => !todo.done).length,
+  };
+};
